47f63efe0f6c5c212f3b1e462bc6b89e7432741d
Jhettoo <diego@engineer.com>
2022-10-07 16:11:33 -0500

    Agrega tamano

--- src/entreprimos/EntrePrimos.java
+++ src/entreprimos/EntrePrimos.java
@@ -1,4 +1,7 @@
 package entreprimos;
+
+import java.util.Scanner;
+
 /**
  *
  * @author diego
@@ -7,14 +10,20 @@ public class EntrePrimos {
 
     public static void main(String[] args) 
     {
+        Scanner scanner = new Scanner(System.in);
         int iteraciones=0;
-        System.out.print("NUMEROS PRIMOS\n\n2 ");
-        for (int i=3; i<100; i=i+2)
+        int tamano=0;
+        System.out.print("NUMEROS PRIMOS\n\n");
+        System.out.print("Ingrese la cantidad de números: ");
+        tamano= scanner.nextInt();
+        System.out.print("2 ");
+        for (int i=3; i<tamano; i=i+2)
         {
             iteraciones++;
             if (  ((i-((int)(i/10))*10)!=5) &&  ((i%3)!=0) &&  ((i%7)!=0) || i==3 || i==5 || i==7 )
             {
-                System.out.print(i+" ");
+                System.out.print(i);
+                System.out.print(" ");
             }
         }
         System.out.println("\n** "+iteraciones+" Iteraciones  **");
